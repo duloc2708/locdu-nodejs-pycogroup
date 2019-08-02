202f95d343b9e5ad1851fc996983f48ae1d30923
duloc2708 <duloc2708@gmail.com>
2019-08-02 11:04:02 +0700

    remove file not use

--- question3/index.js
+++
@@ -1,9 +0,0 @@
-const { load, store } = require('./listFunction')
-let a = load("key1=value1;key2=value2\nkeyA=valueA\n")
-let text = store(a);
-console.log('LOAD DATA:');
-console.log(text);
-
-console.log('LOAD STRING:');
-console.log(a);
-
