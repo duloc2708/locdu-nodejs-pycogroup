e2e346f5119cce73af43d42e8ddcf52a37be9cd5
duloc2708 <duloc2708@gmail.com>
2019-08-06 13:17:01 +0700

    update clear code

--- question3/services/index.js
+++ question3/services/index.js
@@ -5,15 +5,20 @@ const createBill = (listUser) => {
     let listResultUser = []
     listUser.map(item => {
         let { id, name, type, totalBill } = item
+        let discountOnBill100 = 0, discountByUser = 0, totalMoney = 0
         // create modal user, bill
         let user = new User(`user-${id}`, name, type),
             bill = new Bill(`order-user-${id}`, totalBill);
-        // create discount bill on 100$ and discount each user type
-        let discountOnBill100 = totalBill >= 100 ? bill.discountOnBill100(DISCOUNT_BILL_ON_100) : 0,
-            discountByUser = bill.discountByTypeUser(DISCOUNT_BY_USER[type]) || 0;
+        // cal discount bill on 100$ 
+        if (totalBill >= 100) {
+            discountOnBill100 = bill.discountOnBill100(DISCOUNT_BILL_ON_100) || 0
+        }
+        // cal discount each user type
+        discountByUser = bill.discountByTypeUser(DISCOUNT_BY_USER[user.type]) || 0;
+
         // cal total money each user
-        let totalMoney = bill.calNetpayment(discountOnBill100, discountByUser)
-        listResultUser.push({ name: user.name, totalMoney})
+        totalMoney = bill.calNetpayment(discountOnBill100, discountByUser)
+        listResultUser.push({ name: user.name, totalMoney })
     })
     return listResultUser
 }
